fa81332920c32f93aa8a014b4c6544cd095cf7f8
Kiash254 <codewithkiash@gmail.com>
2023-11-14 19:23:11 +0300

    added knowledge files

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+#import settings
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('api.urls',namespace='api')),
     
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # new
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # new
